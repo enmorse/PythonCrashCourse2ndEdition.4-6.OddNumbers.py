5c6a4dcae91ea1f313102f7f85733d13dbc37bac
Morse <enmorse48@gmail.com>
2021-09-23 23:52:44 -0700

    Initial commit

---
+++ Main.py
@@ -0,0 +1,10 @@
+# Use the third argument of the range() function to
+# make a list of the odd numbers from 1 to 20. Use a
+# for loo to print each number.
+
+odd_numbers = []
+
+for value in range(1, 20, 2):
+    odd_numbers.append(value)
+
+print(odd_numbers)
